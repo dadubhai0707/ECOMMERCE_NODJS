8e117ea9f4847090fd5ea4c1270baf0ab2ff860c
dadubhai0707 <sanjaysuthar121212@gmail.com>
2025-11-04 18:22:25 +0530

    order and user model complete

--- model/order.model.js
+++ model/order.model.js
@@ -1,9 +1,75 @@
 const mongoose = require("mongoose")
-
+const ApiError = require("../utils/apiError")
 
 const orderSchema = new mongoose.Schema({
+    User: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    Items: [
+        {
+            Product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true
+            },
+            Quantity: {
+                type: Number,
+                require: true,
+                min: [1, "minimum 1 product buy"]
+            },
+            UnitPrice: {
+                type: Number,
+                required: true,
+            },
+            Total: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    Total: {
+        type: Number,
+        required: true
+    },
+    Status: {
+        type: String,
+        enum: [
+            "Pending",
+            "Confirmed",
+            "In Process",
+            "Shipped",
+            "Delivered",
+            "Cancelled"
+        ],
+        default: "Pending"
+    },
+    PaymentMethod: {
+        type: String,
+        enum: ["COD", "Online"],
+        default: "COD",
+    },
+    PaymentStatus: {
+        type: String,
+        enum: ["Unpaid", "Paid", "Refunded"],
+        default: "Unpaid",
+    }
+},
+    { timestamps: true }
+)
 
+orderSchema.pre("save", function (next) {
+    if (this.Items && this.Items.length > 0) {
+        let Total = 0
+        for (let i = 0; i < this.Items.length; i++) {
+            this.Items[0].Total = this.Items[0].UnitPrice * this.Items[0].Quantity
+            Total += this.Items[0].Total
+        }
+    } else {
+        next(new ApiError(401, "Product Not Found"))
+    }
 })
 
-const Order = mongoose.Schema("Order", orderSchema)
+const Order = mongoose.model("Order", orderSchema)
 module.exports = Order
--- model/user.model.js
+++ model/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
-
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const userSchema = new mongoose.Schema(
     {
         FullName: {
@@ -66,6 +67,35 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true }
 )
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("Password")) return next()
+    this.Password = await bcrypt.hash(this.Password, 12)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (Password) {
+    return await bcrypt.compare(Password, this.Password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        type: "user"
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    )
+}
+
+userSchema.method.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    )
+}
 
 const User = mongoose.model("User", userSchema)
 module.exports = User
