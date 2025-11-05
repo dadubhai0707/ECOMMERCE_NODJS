738e4fac0d81a101ac66aafe53d75cb8dd850962
dadubhai0707 <sanjaysuthar121212@gmail.com>
2025-11-05 20:13:14 +0530

    Express Validatoor

--- Backend/controller/auth/RegisterUser.js
+++ Backend/controller/auth/RegisterUser.js
@@ -3,8 +3,16 @@ const User = require("../../model/user.model")
 const ApiResponse = require("../../utils/apiResponse")
 const ApiError = require("../../utils/apiError")
 const asyncHandler = require("../../utils/asyncHandle")
-
+const { validationResult } = require("express-validator")
 const Register = asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            success: false,
+            errors: errors.array(),
+        });
+    }
+
     const { FullName, Email, Phone, Password, Gender, Address } = req.body
     console.log(req.body)
     return res.json(new ApiResponse(200, req.body, "Success"))
--- Backend/router/auth/authRouter.js
+++ Backend/router/auth/authRouter.js
@@ -2,6 +2,34 @@ const express = require("express")
 const authRouter = express.Router()
 const { Register } = require("../../controller/auth/RegisterUser")
 
-authRouter.post("/register", Register)
+const { body } = require("express-validator");
+authRouter.post(
+    "/register",
+    [
+        body("FullName.FirstName")
+            .isLength({ min: 3 })
+            .withMessage("First name must be at least 3 characters long"),
+        body("FullName.LastName")
+            .notEmpty()
+            .withMessage("Last name is required"),
+        body("Phone")
+            .isLength({ min: 10, max: 10 })
+            .withMessage("Phone number must be 10 digits"),
+
+        body("Password")
+            .isLength({ min: 6 })
+            .withMessage("Password must be at least 6 characters"),
+
+        body("Gender")
+            .isIn(["Male", "Female", "Other"])
+            .withMessage("Gender must be Male, Female or Other"),
+
+        body("Address.*.City")
+            .notEmpty()
+            .withMessage("City is required"),
+
+    ],
+    Register
+)
 
 module.exports = authRouter
